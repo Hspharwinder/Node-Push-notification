3de597c8bc7c183125e6c9881fc6515b87b77060
harwinder <harwinder@zapbuild.com>
2021-10-05 15:17:20 +0530

    getting vapid key

--- server.js
+++ server.js
@@ -1,11 +1,18 @@
 const express = require('express')
+const webpush = require('web-push');
 const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+    const vapidKey = webpush.generateVAPIDKeys();
+    res.send(vapidKey);
 })
+console.log("vapidKey ******** ", webpush.generateVAPIDKeys());
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
+    console.log(`Example app listening at http://localhost:${port}`)
+});
+
+
+// Step 1: npm i web-push "web-push": "^3.4.5"
+// Step 2: work in angular project
